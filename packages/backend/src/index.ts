5e5407dc74860cdfa3f49632a35c53fb866af30c
Carlos Barajas <carlos@glutgut.com>
2026-03-03 21:58:40 -0800

    feat: setup github catalog and org configuration

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -34,6 +34,8 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
